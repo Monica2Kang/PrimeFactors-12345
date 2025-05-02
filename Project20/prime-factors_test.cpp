6bb827c338ef41d5512b8e5ad193ebe994272f4d
monica kang <aksenwkd92@naver.com>
2025-05-02 13:52:11 +0900

    [feature] 33

--- Project20/prime-factors_test.cpp
+++ Project20/prime-factors_test.cpp
@@ -1,8 +1,9 @@
 #include "gmock/gmock.h"
 #include "prime-factors.cpp"
 
-TEST(PrimeFactors, PrimeTest) {
-	EXPECT_EQ(1, 1);
-	EXPECT_EQ(1, 1);
-	EXPECT_EQ(1, 1);
+
+TEST(PrimeFactors, Of1) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {};
+	EXPECT_EQ(expected, prime_factor.of(1));
 }
